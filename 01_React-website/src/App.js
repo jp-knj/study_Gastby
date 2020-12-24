4680ec957b47023ffece2d890bf2f76a4224a429
kenji-0801 <70939128+kenji-0801@users.noreply.github.com>
2020-12-25 00:45:58 +0900

    :art: Add Banner and Header
    t

---
+++ 01_React-website/src/App.js
@@ -0,0 +1,16 @@
+import React from "react";
+import "./styles/App.scss";
+import Header from "./components/header";
+import Banner from "./components/banner";
+
+
+function App() {
+  return (
+    <div className='App'>
+      <Header />
+      <Banner />
+    </div>
+  );
+}
+
+export default App;
