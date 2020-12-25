06d0bb6888949e39511818a4f0201de53573d1d1
kenji-0801 <70939128+kenji-0801@users.noreply.github.com>
2020-12-25 16:05:01 +0900

    :art: Add Cases
    t

--- 01_React-website/src/components/cases.js
+++ 01_React-website/src/components/cases.js
@@ -1,49 +1,52 @@
-import React from 'react';
-import { ReactComponent as CasesPrev } from "../assets/arrow-right.svg";
-import { ReactComponent as CasesNext } from "../assets/arrow-left.svg";
+import React from "react";
+import { ReactComponent as CasesNext } from "../assets/arrow-right.svg";
+import { ReactComponent as CasesPrev } from "../assets/arrow-left.svg";
 
 const caseStudies = [
   {
     id: 1,
     subtitle: "Curology",
-    title: "A custom formula for your skin's unique needs",
-    img: "curology-min",
+    title: "A custom formula for your skin’s unique needs",
+    img: "curology-min"
   },
   {
     id: 2,
     subtitle: "Yourspace",
-    title: "A custom formula for your skin's unique needs",
-    img: "yourspace-min",
+    title: "Open space floor plans for you next venture",
+    img: "yourspace-min"
   },
   {
     id: 3,
     subtitle: "Lumin",
-    title: "A custom formula for your skin's unique needs",
-    img: "lumin-min",
+    title: "For your best look ever",
+    img: "lumin-min"
   }
-]
+];
 
 const Cases = () => {
   return (
-    <section className="cases">
-      <div className="container-fluid">
-        <div className="cases-navigation">
-          <div className="cases-arrow prev disabled">
+    <section className='cases'>
+      <div className='container-fluid'>
+        <div className='cases-navigation'>
+          <div className='cases-arrow prev disabled'>
             <CasesPrev />
           </div>
-          <div className="cases-arrow next disabled">
+          <div className='cases-arrow next'>
             <CasesNext />
           </div>
         </div>
-        <div className="row">
+        <div className='row'>
           {caseStudies.map(caseItem => (
-            <div className="case" key={caseItem.id}>
-              <div className="case-details">
+            <div className='case' key={caseItem.id}>
+              <div className='case-details'>
                 <span>{caseItem.subtitle}</span>
                 <h2>{caseItem.title}</h2>
               </div>
-              <div className="case-iamge">
-                <img src={require(`../assets/${caseItem.img}.png`)} alt={caseItem.title} />
+              <div className='case-image'>
+                <img
+                  src={require(`../assets/${caseItem.img}.png`)}
+                  alt={caseItem.title}
+                />
               </div>
             </div>
           ))}
