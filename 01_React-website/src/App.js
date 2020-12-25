4407dd9e671eca1bc3cf1dfca5666ab855ed289b
kenji-0801 <70939128+kenji-0801@users.noreply.github.com>
2020-12-25 19:06:31 +0900

    :art: Add Navigation
    t

--- 01_React-website/src/App.js
+++ 01_React-website/src/App.js
@@ -5,19 +5,17 @@ import "./styles/App.scss";
 import Header from "./components/header";
 
 // pages
-import Home from "./pages/home";
-import Members from "./pages/members";
 import Activity from "./pages/activity";
+import Members from "./pages/members";
+import Home from "./pages/home";
 
-// routes
 const routes = [
-  { path: "/", name: "Home", Components: Home },
-  { path: "/members", name: "members", Components: Members },
-  { path: "/activity", name: "activity", Components: Activity }
-]
+  { path: "/", name: "Home", Component: Home },
+  { path: "/", name: "members", Component: Members },
+  { path: "/about-us", name: "activity", Component: Activity }
+];
 
 function App() {
-
   useEffect(() => {
     // Grab inner height of window for mobile reasons when dealing with vh
     let vh = window.innerHeight * 0.01;
@@ -30,7 +28,7 @@ function App() {
 
   return (
     <>
-      <Header />
+      <Header/>
       <div className='App'>
         {routes.map(({ path, Component }) => (
           <Route key={path} exact path={path}>
---
+++ 01_React-website/src/components/navigation.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+const Navigation = () => {
+  return (
+    <div className="header">
+      <div className="container">
+        <div className="row v-center space-between">
+          <div className="logo"><a href="/">YNC.</a></div>
+          <div className="nav"><span></span><span></span></div>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default Navigation;
