66ca344e00a0410710c111c9481ee4ae81059fb1
kenji-0801 <70939128+kenji-0801@users.noreply.github.com>
2020-12-25 20:25:11 +0900

    :sparkles: Add Styling of Navigation
    t

--- 01_React-website/src/App.js
+++ 01_React-website/src/App.js
@@ -2,25 +2,59 @@ import React, { useEffect } from "react";
 import { Route } from "react-router-dom";
 import { gsap } from "gsap";
 import "./styles/App.scss";
-import Header from "./components/header";
 
-// pages
+// Components
+import Header from "./components/header";
+import Navigation from "./components/navigation";
+
+// Pages
 import Activity from "./pages/activity";
 import Members from "./pages/members";
 import Home from "./pages/home";
 
 const routes = [
   { path: "/", name: "Home", Component: Home },
-  { path: "/", name: "members", Component: Members },
-  { path: "/about-us", name: "activity", Component: Activity }
+  { path: "/members", name: "members", Component: Members },
+  { path: "/activity", name: "activity", Component: Activity }
 ];
 
+function debounce(fn, ms) {
+  let timer;
+  return () => {
+    clearTimeout(timer);
+    timer = setTimeout(() => {
+      timer = null;
+      fn.apply(this, arguments);
+    }, ms);
+  };
+}
+
 function App() {
+
+  const [dimensions, setDimensions] = React.useState({
+    height: window.innerHeight,
+    width: window.innerWidth
+  });
   useEffect(() => {
     // Grab inner height of window for mobile reasons when dealing with vh
     let vh = window.innerHeight * 0.01;
     // Set css variable vh
     document.documentElement.style.setProperty("--vh", `${vh}px`);
+
+    // prevents flashing
+    gsap.to("body", 0, { css: { visibility: "visible" } });
+    const debouncedHandleResize = debounce(function handleResize() {
+      setDimensions({
+        height: window.innerHeight,
+        width: window.innerWidth
+      });
+    }, 1000);
+
+    window.addEventListener("resize", debouncedHandleResize);
+
+    return () => {
+      window.removeEventListener("resize", debouncedHandleResize);
+    }
 
     // prevents flashing
     gsap.to("body", 0, { css: { visibility: "visible" } });
@@ -36,6 +70,7 @@ function App() {
           </Route>
         ))}
       </div>
+      <Navigation />
     </>
   );
 }
--- 01_React-website/src/components/navigation.js
+++ 01_React-website/src/components/navigation.js
@@ -1,16 +1,62 @@
-import React from 'react';
+import React from "react";
+import { NavLink } from "react-router-dom";
 
 const Navigation = () => {
   return (
-    <div className="header">
-      <div className="container">
-        <div className="row v-center space-between">
-          <div className="logo"><a href="/">YNC.</a></div>
-          <div className="nav"><span></span><span></span></div>
+    <nav>
+      <div className='container'>
+        <div className='nav-columns'>
+          <div className='nav-column'>
+            <div className='nav-label'>Menu</div>
+            <ul className='nav-links'>
+              <li>
+                <NavLink to='/activity' exact>
+                  Activity
+                </NavLink>
+              </li>
+              <li>
+                <NavLink to='/members' exact>
+                  Members
+                </NavLink>
+              </li>
+            </ul>
+          </div>
+          <div className='nav-column'>
+            <div className='nav-label'>Contact</div>
+            <div className='nav-infos'>
+              <ul className='nav-info'>
+                <li className='nav-info-label'>Email</li>
+                <li>
+                  <NavLink to='/contact' exact>
+                    Get in touch with us
+                  </NavLink>
+                </li>
+                <li>
+                  <NavLink to='/audit' exact>
+                    Get a free audit
+                  </NavLink>
+                </li>
+              </ul>
+              <ul className='nav-info'>
+                <li className='nav-info-label'>Headquarter</li>
+                <li>Route du Jura 49</li>
+                <li>1700 Fribourg</li>
+                <li>Switzerland</li>
+              </ul>
+              <ul className='nav-info'>
+                <li className='nav-info-label'>Phone</li>
+                <li>+41 (0) 79 510 28 70</li>
+              </ul>
+              <ul className='nav-info'>
+                <li className='nav-info-label'>Legal</li>
+                <li>Privacy & Cookies</li>
+              </ul>
+            </div>
+          </div>
         </div>
       </div>
-    </div>
-  )
-}
+    </nav>
+  );
+};
 
 export default Navigation;
