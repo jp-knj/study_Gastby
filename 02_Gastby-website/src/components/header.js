20d272e9ab66e1059a7c7b00f842c79f9949a9a4
kenji-0801 <70939128+kenji-0801@users.noreply.github.com>
2020-12-26 14:35:57 +0900

    :sparkles: Add globalStyles
    t

--- 02_Gastby-website/src/components/header.js
+++ 02_Gastby-website/src/components/header.js
@@ -2,9 +2,16 @@ import React from 'react'
 
 // Styled Components
 import { HeaderNav } from '../styles/headerStyles';
+import { Container } from '../styles/globalStyles';
 
 const Header = () => {
-  return <HeaderNav>Header</HeaderNav>;
+  return (
+    <HeaderNav>
+      <Container>
+
+      </Container>
+    </HeaderNav>
+  );
 }
 
 export default Header;
