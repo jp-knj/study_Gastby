9e4e129cffb9e970b93e5a9b5d399c5910f45cb5
kenji-0801 <70939128+kenji-0801@users.noreply.github.com>
2020-12-26 14:24:24 +0900

    :art: Add HeaderNav
    t

--- 02_Gastby-website/src/components/header.js
+++ 02_Gastby-website/src/components/header.js
@@ -1,7 +1,10 @@
 import React from 'react'
 
+// Styled Components
+import { HeaderNav } from '../styles/headerStyles';
+
 const Header = () => {
-  return <div>Header</div>;
+  return <HeaderNav>Header</HeaderNav>;
 }
 
 export default Header;
---
+++ 02_Gastby-website/src/styles/headerStyles.js
@@ -0,0 +1,6 @@
+import styled from 'styled-components'
+
+export const HeaderNav = styled.div`
+  background-color: blueviolet;
+  color: #fff;
+`
