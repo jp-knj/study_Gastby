ce373e76b55a04300ac8ccd3a5170f9d5a5711df
kenji-0801 <70939128+kenji-0801@users.noreply.github.com>
2020-12-28 21:26:05 +0900

    :sparkles: Add animation using GSAP
    t

--- 05_Hamburger-menu-website/src/components/Hamburger.js
+++ 05_Hamburger-menu-website/src/components/Hamburger.js
@@ -1,35 +1,101 @@
 import React, { useEffect, useRef } from "react";
 import { Link } from "react-router-dom";
+import gsap from "gsap";
 
 const Hamburger = ({ state }) => {
+  // Vars for our animated dom nodes
   let menu = useRef(null);
+  let revealMenu = useRef(null);
+  let revealMenuBackground = useRef(null);
+  let cityBackground = useRef(null);
+  let line1 = useRef(null);
+  let line2 = useRef(null);
+  let line3 = useRef(null);
+  let info = useRef(null);
 
   useEffect(() => {
     if (state.clicked === false) {
       // Close our Menu
-      menu.style.display = 'none'
+      gsap.to([revealMenu, revealMenuBackground], {
+        duration: 0.8,
+        height: 0,
+        ease: "power3.inOut",
+        stagger: {
+          amount: 0.4
+        }
+      });
+      gsap.to(menu, {
+        duration: 1,
+        css: { display: "none" }
+      });
     } else if (state.clicked === true || state.clicked === true && state.initial === null){
       // Open our Menu
-      menu.style.display = 'block';
+      gsap.to(menu, {
+        duration: 0,
+        css: { display: "block" }
+      });
+      gsap.to([revealMenuBackground,revealMenu], {
+        duration: 0,
+        opacity: 1,
+        height: "100%"
+      });
+      staggerReveal([revealMenuBackground, revealMenu]);
+      fadeInUp(info);
+      staggerText(line1, line2,line3)
+    }
+  },[state]);
+
+  const staggerReveal = (node1, node2) => {
+    gsap.from([node1, node2], {
+      duration: 0.8,
+      height: 0,
+      transformOrigin: "right top",
+      skewY: 3,
+      ease: "power3.inOut",
+      stagger: {
+        amount: 0.3
+      }
+    });
   }
-});
+
+  const fadeInUp = (node) => {
+    gsap.from(node, {
+      y: 60,
+      duration: 1,
+      delay: 0.2,
+      opacity: 0,
+      ease: "power3.inOut",
+    });
+  };
+
+  const staggerText = (node1, node2, node3) => {
+    gsap.from([node1, node2, node3], {
+      y: 100,
+      duration: 1,
+      delay: 0.4,
+      ease: "power3.inOut",
+      stagger: {
+        amount: 0.3
+      }
+    });
+  };
 
 return (
   <div ref={el => (menu = el)} className='hamburger-menu'>
-    <div className='menu-secondary-background-color'></div>
-    <div className='menu-layer'>
+    <div ref={el => (revealMenuBackground = el)} className='menu-secondary-background-color'></div>
+    <div ref={el => (revealMenu = el)} className='menu-layer'>
       <div className='menu-city-background'></div>
       <div className='container'>
         <div className='wrapper'>
           <div className='menu-links'>
             <nav>
               <ul>
-                <li><Link to='/opportunities'>Opportunities</Link></li>
-                <li><Link to='/solutions'>Solutions</Link></li>
-                <li><Link to='/contact-us'>Contact us</Link></li>
+                <li><Link ref={el => (line1 = el)}  to='/opportunities'>Opportunities</Link></li>
+                <li><Link ref={el => (line2 = el)}  to='/solutions'>Solutions</Link></li>
+                <li><Link ref={el => (line3 = el)}  to='/contact-us'>Contact us</Link></li>
               </ul>
             </nav>
-            <div className='info'>
+            <div ref={el => (info = el)} className='info'>
               <h3>Our Promise</h3>
               <p>
                 The passage experienced a surge in popularity during the 1960s
