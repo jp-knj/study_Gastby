0df96324799f189ac257088652c4ad38d5ac60a9
kenji-0801 <70939128+kenji-0801@users.noreply.github.com>
2020-12-28 18:25:21 +0900

    :art: Updated Markup on hamburger
    t

--- 05_Hamburger-menu-website/src/components/Hamburger.js
+++ 05_Hamburger-menu-website/src/components/Hamburger.js
@@ -2,29 +2,38 @@ import React from "react";
 import { Link } from "react-router-dom";
 
 const Hamburger = () => {
-  return (
-    <div className='hamburger-menu'>
-      <div className='menu-secondary-background-color'></div>
-      <div className='menu-layer'>
-        <div className='menu-city-background'>
-
-        </div>
-      </div>
+return (
+  <div className='hamburger-menu'>
+    <div className='menu-secondary-background-color'></div>
+    <div className='menu-layer'>
+      <div className='menu-city-background'></div>
       <div className='container'>
         <div className='wrapper'>
           <div className='menu-links'>
             <nav>
               <ul>
                 <li><Link to='/opportunities'>Opportunities</Link></li>
-                <li><Link to='/Solutions'>Solutions</Link></li>
-                <li><Link to='/contact-us'>Contact Us</Link></li>
+                <li><Link to='/solutions'>Solutions</Link></li>
+                <li><Link to='/contact-us'>Contact us</Link></li>
               </ul>
             </nav>
+            <div className='info'>
+              <h3>Our Promise</h3>
+              <p>
+                The passage experienced a surge in popularity during the 1960s
+                when Letraset used it on their dry-transfer sheets, and again
+                during the 90s as desktop publishers bundled the text with
+                their software.
+                </p>
+            </div>
+            <div className='locations'>
+              Locations:
+            </div>
           </div>
         </div>
       </div>
     </div>
-  )
-};
+  </div>
+)};
 
 export default Hamburger;
