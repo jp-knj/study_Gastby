f2eef56e93d5bd54cd79c55965c48ead32ee4cf3
kenji-0801 <70939128+kenji-0801@users.noreply.github.com>
2020-12-30 17:57:37 +0900

    :sparkles: Add data of slide and images
    t

---
+++ 07_estate-website/src/data/SliderData.js
@@ -0,0 +1,26 @@
+export const SliderData = [
+  {
+    title: 'Luxury Villa in Bali, Indonesia',
+    price: '$4,280,000',
+    label: 'View Home',
+    image: ImageOne,
+    path: '/about',
+    alt: 'House'
+  },
+  {
+    title: 'House in San Jone, San Jone',
+    price: '$4,280,000',
+    label: 'View Home',
+    image: ImageTwo,
+    path: '/works',
+    alt: 'Works'
+  },
+  {
+    title: 'House in Calabasas, Calabasas',
+    price: '$4,280,000',
+    label: 'View Home',
+    image: ImageThree,
+    path: '/blog',
+    alt: 'Blog'
+  }
+]
