cba37dba44894ab4e745bc1f5a5ad06157154a07
kenji-0801 <70939128+kenji-0801@users.noreply.github.com>
2020-12-30 18:19:13 +0900

    :art: Markup Hero Section
    t

--- 07_estate-website/src/components/Hero.js
+++ 07_estate-website/src/components/Hero.js
@@ -1,5 +1,9 @@
 import React from 'react'
+import { FaSlideshare } from 'react-icons/fa';
 import styled, { css } from "styled-components/macro";
+import { Button } from './Button';
+import { IoMdArrowRoundForward } from "react-icons/io";
+import { IoArrowForward, IoArrowBack } from "react-icons/io5";
 
 const HeroSection = styled.section`
   height: 100vh;
@@ -18,11 +22,46 @@ const HeroWrapper = styled.div`
   position: relative;
 `
 
-const Hero = () => {
+const HeroSlide = styled.div`
+
+`
+
+const HeroSldier = styled.div`
+
+`
+
+const HeroImage = styled.img`
+
+`
+
+const HeroContent = styled.div`
+`
+
+const Arrow = styled(IoMdArrowRoundForward)``;
+
+const SliderButtons = styled.div``;
+const PrevArrow = styled.div``;
+const NextArrow = styled.div``;
+
+const Hero = ({ slides }) => {
   return (
     <HeroSection>
       <HeroWrapper>
-        <h1>Hero Hello World</h1>
+        {slides.map((slide, index) => {
+          return (
+            <HeroSlide key={index}>
+              <HeroSldier>
+                <HeroImage />
+                <HeroContent>
+                  <h1>{slide.title}</h1>
+                  <p>{slide.price}</p>
+                  <Button to={slide.path} primary="true">{slide.label}<Arrow/></Button>
+                </HeroContent>
+              </HeroSldier>
+            </HeroSlide>
+          )
+        })}
+        <SliderButtons><PrevArrow /><NextArrow /></SliderButtons>
       </HeroWrapper>
     </HeroSection>
   )
