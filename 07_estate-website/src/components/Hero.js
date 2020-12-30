1406e5d5b01e53ef899abacb251acb0db9985182
kenji-0801 <70939128+kenji-0801@users.noreply.github.com>
2020-12-30 18:49:00 +0900

    :sparkles: Styling Hero section
    t

--- 07_estate-website/src/components/Hero.js
+++ 07_estate-website/src/components/Hero.js
@@ -23,25 +23,106 @@ const HeroWrapper = styled.div`
 `
 
 const HeroSlide = styled.div`
-
-`
+  z-index: 1;
+  width: 100%;
+  height: 100%;
+`;
 
 const HeroSldier = styled.div`
+  position: absolute;
+  top: 0;
+  left: 0;
+  width:100%;
+  height: 100%;
+  display: flex;
+  align-items: center;
+  justify-content: center;
 
+  &::before {
+    content: '';
+    position: absolute;
+    z-index: 2;
+    width: 100%;
+    height: 100vh;
+    bottom: 0vh;
+    left: 0;
+    overflow: hidden;
+    background-color: linear-gradient(
+      0deg,
+      rgba (0, 0, 0, 0.2) 0%,
+      rgba (0, 0, 0, 0.2) 50%,
+      rgba (0, 0, 0, 0.6) 100%,
+    )
+  }
 `
 
 const HeroImage = styled.img`
-
+  position: absolute;
+  top: 0;
+  left: 0;
+  width: 100vw;
+  height: 100vh;
+  object-fit: cover;
 `
 
 const HeroContent = styled.div`
+  position: relative;
+  z-index: 10;
+  display: flex;
+  flex-direction: column;
+  max-width: 1600px;
+  width: calc(100% - 100px);
+  color: #fff;
+
+  h1 {
+    font-size: clamp(1rem, 8vw, 2rem);
+    font-weight: 400;
+    text-transform: uppercase;
+    text-shadow: 0px 0px 20px rgba(0,0,0,0.4);
+    text-align: left;
+    margin-bottom: .8rem;
+  }
+  p {
+    margin-bottom: 1.2rem;
+    text-shadow: 0px 0px 20px rgba(0,0,0,0.4);
+  }
 `
 
 const Arrow = styled(IoMdArrowRoundForward)``;
 
-const SliderButtons = styled.div``;
-const PrevArrow = styled.div``;
-const NextArrow = styled.div``;
+const arrowButtons = css`
+  height: 50px;
+  width: 50px;
+  color: white;
+  cursor: pointer;
+  background-color: #000d1a;
+  border-radius: 50px;
+  padding: 10px;
+  margin-right: 1rem;
+  user-select: none;
+  transition: .3s;
+
+  &:hover {
+    background-color: #cd853f;
+    transform: scale(1.05);
+  }
+`;
+
+const PrevArrow = styled(IoArrowBack)`
+  ${arrowButtons};
+`;
+
+const NextArrow = styled(IoArrowForward)`
+  ${arrowButtons};
+`;
+
+const SliderButtons = styled.div`
+  position: absolute;
+  bottom: 50px;
+  right: 50px;
+  display: flex;
+  z-index: 10;
+`;
 
 const Hero = ({ slides }) => {
   return (
@@ -51,7 +132,7 @@ const Hero = ({ slides }) => {
           return (
             <HeroSlide key={index}>
               <HeroSldier>
-                <HeroImage />
+                <HeroImage src={slide.image} alt={ slide.alt}/>
                 <HeroContent>
                   <h1>{slide.title}</h1>
                   <p>{slide.price}</p>
