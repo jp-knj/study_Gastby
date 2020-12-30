7a5b7978712ffa530b501fc57ad11a7d09bb1642
kenji-0801 <70939128+kenji-0801@users.noreply.github.com>
2020-12-30 17:37:16 +0900

    :sparkles: Add Hero section
    t

--- 07_estate-website/src/App.js
+++ 07_estate-website/src/App.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import Hero from './components/Hero';
 import Navbar from './components/Navbar';
 import GlobalStyle from './globalStyles';
 
@@ -6,7 +7,8 @@ function App() {
   return (
     <>
       <GlobalStyle/>
-      <Navbar/>
+      <Navbar />
+      <Hero />
     </>
   );
 }
