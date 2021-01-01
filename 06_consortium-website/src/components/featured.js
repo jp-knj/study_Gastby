4442e8f2ee13ec693c2c32ad56b004ead3ec3035
kenji-0801 <70939128+kenji-0801@users.noreply.github.com>
2021-01-01 23:22:58 +0900

    :tada: Update react-app
    t

---
+++ 06_consortium-website/src/components/featured.js
@@ -0,0 +1,26 @@
+import React from "react";
+
+const Featured = () => {
+  return (
+    <section className='main'>
+      <div className='container'>
+        <div className='row'>
+          <h2>
+            <div className='line'>
+              <span>Creating unique brands is</span>
+            </div>
+            <div className='line'>
+              <span>what we do.</span>
+            </div>
+          </h2>
+          <div className='btn-row'>
+            Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchange
+
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default Featured;
