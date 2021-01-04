2cd6453c1ca1d4dd616537b0dcd9864a410e3db0
kenji-0801 <70939128+kenji-0801@users.noreply.github.com>
2021-01-04 17:08:15 +0900

    :+1: Add About section
    t

---
+++ 12_consortium-website/src/components/home/about.js
@@ -0,0 +1,23 @@
+import React from 'react'
+
+export const About = () => {
+  return (
+    <section className='about'>
+      <div className='about_inner'>
+        <div className='about_text'>
+          <p className='background'>What's our activity ?</p>
+        </div>
+        <div className='about_meta'>
+          <h2 className='about_title'>
+            About Us
+          </h2>
+          <p className='about_description'>
+            こちらはテキストになります。こちらはテキストになります。こちらはテキストになります。こちらはテキストになります。こちらはテキストになります。こちらはテキストになります。こちらはテキストになります。こちらはテキストになります。こちらはテキストになります。こちらはテキストになります。こちらはテキストになります。こちらはテキストになります。
+          </p>
+        </div>
+      </div>
+    </section>
+  )
+}
+
+export default About;
